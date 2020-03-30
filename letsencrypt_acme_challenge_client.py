4b86d62b34e53a64cf63e9deaff7d89ff11263ee
ckaufmann <cedrik.kaufmann@gmail.com>
2020-03-30 10:07:03 +0200

    update wildcard acme challenge

--- letsencrypt_acme_challenge_client.py
+++ letsencrypt_acme_challenge_client.py
@@ -1,5 +1,3 @@
-#!venv/bin/python
-
 import argparse
 from netcup_api.dns import Client
 from netcup_api.dns import DNSRecord
@@ -11,19 +9,19 @@ def main():
     parser.add_argument('--password', action='store', type=str, help='API-Password to access Netcup DNS API',
                         required=True)
     parser.add_argument('--customernumber', action='store', type=str, help='Netcup customer number', required=True)
-    parser.add_argument('--certbot_domain', action='store', type=str, help='Let´s encrypt domain', required=True)
-    parser.add_argument('--certbot_validation', action='store', type=str, help='Let´s encrypt challenge', required=True)
-    parser.add_argument('--domain', action='store', type=str, help='Domain name which should be accessed',
+    parser.add_argument('--domain', action='store', type=str, help='Domain to issue wildcard certificate for',
                         required=True)
+    parser.add_argument('--certbot_validation', action='store', type=str, help='Let´s encrypt challenge', required=True)
     parser.add_argument('--cleanup', action='store_true')
 
     args = parser.parse_args()
+
+    acme_challenge = '_acme-challenge'
 
     client = Client(args.key, args.password, args.customernumber)
 
     if args.cleanup:
         records = client.get_dns_records(args.domain)
-        acme_challenge = f'_acme-challenge.{args.certbot_domain}'
 
         for r in records:
             if r.hostname == acme_challenge:
@@ -32,7 +30,7 @@ def main():
 
     else:
         challenge_record = DNSRecord(
-            hostname=f'_acme-challenge.{args.certbot_domain}', record_type='TXT', destination=args.certbot_validation
+            hostname=acme_challenge, record_type='TXT', destination=args.certbot_validation
         )
 
         client.add_dns_records(args.domain, challenge_record)
